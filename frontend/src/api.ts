6340c03de737bba58358e6e0873c31280234037c
Ivan Pidopryhora <ivanpidopryhora13@gmail.com>
2026-09-22 17:47:06 +0300

    fix import 2

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,4 +1,4 @@
-import type { HealthReport } from './interfaces/responses'
+import {HealthReport} from "./interfaces/responses.ts";
 
 const API_BASE = import.meta.env.VITE_API_URL ?? 'http://localhost:8000'
 
